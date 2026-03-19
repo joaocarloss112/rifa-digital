c9502eb27a45da5fc392bd3a016332d3dd90917b
joaocarloss112 <135226719+joaocarloss112@users.noreply.github.com>
2026-03-19 11:48:50 +0000

    main, comprador e adicionando um dicionario pra rifa

--- src/criar_rifa.py
+++ src/criar_rifa.py
@@ -4,14 +4,23 @@ def Criar_rifa():
     nome_rifa = str(input("Informe o nome da rifa a ser criada: "))
     descricao = str(input("Informe uma breve descrição da rifa: "))
     qtd = int(input("Informe a quantidade de números da rifa: "))
-    preco = int("Informe o preço da rifa: ")
+    preco = int(input("Informe o preço da rifa: "))
     data = input("Digite a data do sorteio (DD/MM/AAAA): ")
     try:
         data_objeto = datetime.strptime(data_usuario, "%d/%m/%Y")
         print(f"Data validada com sucesso: {data_objeto}")
+        rifa = {
+            "nome": nome_rifa,
+            "descricao": descricao,
+            "quantidade": qtd,
+            "preco": preco,
+            "data": data_objeto,
+            "numeros_disponiveis": list(range(1, qtd + 1)),
+            "compradores": {} 
+        }
+    return rifa
     except ValueError:
         print("Formato inválido! Use o padrão DD/MM/AAAA.")
-    pass
 
 def verificar_criacao(nome, descricao, qtd, preco, data):
     return {
