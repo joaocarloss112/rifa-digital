ff2507c52367648a051e51bdba0452495dfb326b
Igor Uchôa <igor.00000848631@unicap.br>
2026-03-22 14:35:00 +0000

    update-Comprador

--- src/comprador.py
+++ src/comprador.py
@@ -1,7 +1,17 @@
-def comprar_numero(rifa, numero, nome_comprador):
-    if numero in rifa["numeros_disponiveis"]:
-        rifa["numeros_disponiveis"].remove(numero)
-        rifa["compradores"][numero] = nome_comprador
-        return True
-    else:
+def reservar_numero(rifa, numero, nome, telefone, email=None):
+    if numero not in rifa["numeros"]:
         return False
+
+    if rifa["numeros"][numero]["status"] != "disponivel":
+        return False
+
+    rifa["numeros"][numero] = {
+        "status": "reservado",
+        "comprador": {
+            "nome": nome,
+            "telefone": telefone,
+            "email": email
+        }
+    }
+
+    return True
